ca5d30539037e1042a083821e0ba2cdb77e29263
wgoode3 <wgoode3@gmail.com>
2018-02-25 01:24:38 -0500

    >added greentext

--- apps/forum_app/static/forum_app/js/script.js
+++ apps/forum_app/static/forum_app/js/script.js
@@ -1,5 +1,7 @@
 var img = document.getElementsByTagName("img");
+var pre = document.getElementsByTagName("pre");
 var imageInput = document.querySelector('input[name="image"]')
+
 for(let i of img){
     i.addEventListener("click", function(e){
         let temp = e.target.dataset.altSrc;
@@ -22,4 +24,27 @@ function previewFile() {
     if (file) {
         reader.readAsDataURL(file);
     }
+}   
+
+// generate green text and links
+
+for(let ele of pre){
+    let el = ele.innerHTML;
+    let lines = el.split("\n");
+    for(let line in lines){
+        if(lines[line].search("&gt;&gt;&gt") > -1){
+            var words = lines[line].split(" ");
+            for(let word in words){
+                if(words[word].search("&gt;&gt;&gt") === 0){
+                    let w = words[word].split("&gt;&gt;&gt;")[1]
+                    words[word] = words[word].replace(`&gt;&gt;&gt;${w}`, `<a href="/post/${w}">>>>${w}</a>`);
+                }
+            }
+            lines[line] = words.join(" ");
+        }else if(lines[line].search("&gt") === 0){
+            lines[line] = `<span style="color: #789922;">${lines[line]}</span>`;
+        }
+    }
+    el = lines.join("\n")
+    ele.innerHTML = el;
 }
--- apps/forum_app/views.py
+++ apps/forum_app/views.py
@@ -3,30 +3,30 @@ from .models import Post
 from django.contrib import messages
 
 def index(request):
-	
-	threads = Post.objects.filter(is_thread=True)
+    
+    threads = Post.objects.filter(is_thread=True)
 
-	for thread in threads:
-		num = len(thread.replies.all())
-		thread.abridged_replies = list(thread.replies.all())[-3:]
-		if num > 3:
-			thread.hidden = num - 3
+    for thread in threads:
+        num = len(thread.replies.all())
+        thread.abridged_replies = list(thread.replies.all())[-2:]
+        if num > 2:
+            thread.hidden = num - 2
 
-	return render(request, "forum_app/index.html", {"posts": threads})
+    return render(request, "forum_app/index.html", {"posts": threads})
 
 def new_thread(request):
-	post = Post.objects.new_thread(request.POST, request.FILES)
-	if type(post) is list:
-		for error in post:
-			messages.add_message(request, messages.ERROR, error)
-	return redirect("/")
+    post = Post.objects.new_thread(request.POST, request.FILES)
+    if type(post) is list:
+        for error in post:
+            messages.add_message(request, messages.ERROR, error)
+    return redirect("/")
 
 def reply(request, post_id):
-	post = Post.objects.new_reply(request.POST, request.FILES, post_id)
-	if type(post) is list:
-		for error in post:
-			messages.add_message(request, messages.ERROR, error)
-	return redirect("/thread/{}".format(post_id))
+    post = Post.objects.new_reply(request.POST, request.FILES, post_id)
+    if type(post) is list:
+        for error in post:
+            messages.add_message(request, messages.ERROR, error)
+    return redirect("/thread/{}".format(post_id))
 
 def thread(request, thread_id):
-	return render(request, "forum_app/thread.html", {"post": Post.objects.get(id=thread_id)})
+    return render(request, "forum_app/thread.html", {"post": Post.objects.get(id=thread_id)})
